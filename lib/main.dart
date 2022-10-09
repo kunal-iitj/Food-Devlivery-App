bf83a4395aee2fe6574e92d963e271beffb991e8
Kunal Tiwari <tiwari.25@iitj.ac.in>
2022-10-09 16:11:48 +0530

    final commit finished the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import "services/login_field.dart";
 
+// done with this project 
+// this is the last commit that would be made
 void main() {
   runApp(const MaterialApp(
     title: 'Login Page',
@@ -19,37 +21,60 @@ class HomeScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.blue,
-        child: Center(
-          child: Container( 
-            width: 400,
-            height: 300,
-            child: Column( 
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-              children: <Widget>[
-                Image.asset('images/flutter-logo.png', width: 50, height: 50,),
-                LoginField(Icon(Icons.person), 'Username'), 
-                LoginField(Icon(Icons.lock), 'Password'),
-                Container(
-                  width: 150,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.pinkAccent,
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
-
-                  
-                  child: TextButton(
-                    onPressed: null, 
-                    child: Text(
-                      'Submit',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0
-                      ),)),
-                ) 
-              ]
-            )
+        child: Stack(children: <Widget>[
+          Align(
+            widthFactor: 0.5,
+            heightFactor: 0.5,
+            alignment: Alignment.bottomRight,
+            child: Material(
+              borderRadius: const BorderRadius.all(Radius.circular(200)),
+              color: Colors.white.withOpacity(0.4),
+              child: Container(
+                width: 400,
+                height: 400
+              )
+            ),
           ),
+           Center(
+            child: Container( 
+              width: 400,
+              height: 300,
+              child: Column( 
+                mainAxisAlignment: MainAxisAlignment.center, 
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LoginField(Icon(Icons.person), 'Username'),
+                  ), 
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: LoginField(Icon(Icons.lock), 'Password'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 150,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.pinkAccent,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))
+                      ),
+        
+                      
+                      child: TextButton(
+                        onPressed: null, 
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0
+                          ),)),
+                    ),
+                  ) 
+                ]
+              )
+            ),
+          ),]
         ),
       )
     );
